4f3850f60fe9c4981337452d377f29cb6bcceb6d
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-10-04 16:38:58 -0400

    build(syntax): export `detectSyntax`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,6 @@
  * @module mlly/lib
  */
 
+export { default as detectSyntax } from './detect-syntax'
 export { default as hasCJSSyntax } from './has-cjs-syntax'
 export { default as hasESMSyntax } from './has-esm-syntax'
