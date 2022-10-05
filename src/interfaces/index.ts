4384be4525b22858e4cbcc782e5a57352583fd60
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-10-05 17:47:26 -0400

    feat(interfaces): `ExportStatement`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -6,4 +6,5 @@
 export type { default as DynamicImport } from './import-dynamic'
 export type { default as StaticImport } from './import-static'
 export type { default as Statement } from './statement'
+export type { default as ExportStatement } from './statement-export'
 export type { default as ImportStatement } from './statement-import'
---
+++ src/interfaces/statement-export.ts
@@ -0,0 +1,33 @@
+/**
+ * @file Interfaces - ExportStatement
+ * @module mlly/interfaces/ExportStatement
+ */
+
+import type { Declaration } from '#src/types'
+import type Statement from './statement'
+
+/**
+ * Export statement object schema.
+ *
+ * @see {@link Statement}
+ *
+ * @extends {Statement}
+ */
+interface ExportStatement extends Statement {
+  /**
+   * Export declaration, if any.
+   */
+  declaration: Declaration | undefined
+
+  /**
+   * Export names.
+   */
+  exports: string[]
+
+  /**
+   * Export statement type.
+   */
+  type: 'declaration' | 'default' | 'named' | 'star'
+}
+
+export type { ExportStatement as default }
