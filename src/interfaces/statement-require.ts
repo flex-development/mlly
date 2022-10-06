0dc6d7a8cb6a4c00f20621a3232cd0b37476025b
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-10-05 21:52:49 -0400

    feat(interfaces): `RequireStatement`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/interfaces/statement-require.ts
@@ -0,0 +1,32 @@
+/**
+ * @file Interfaces - RequireStatement
+ * @module mlly/interfaces/RequireStatement
+ */
+
+import type Statement from './statement'
+
+/**
+ * Require statement object schema.
+ *
+ * @see {@link Statement}
+ *
+ * @extends {Statement}
+ */
+interface RequireStatement extends Statement {
+  /**
+   * Import names.
+   */
+  imports: string[]
+
+  /**
+   * Module specifier.
+   */
+  specifier: NonNullable<Statement['specifier']>
+
+  /**
+   * Import statement type.
+   */
+  type: 'require'
+}
+
+export type { RequireStatement as default }
