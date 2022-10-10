8ff6a944207b32f45eddda8a7fd6ac38bf47f634
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-10-09 20:56:14 -0400

    feat(types): `MIMEType`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,5 +5,6 @@
 
 export type { default as Declaration } from './declaration'
 export type { default as Ext } from './ext'
+export type { default as MIMEType } from './mime-type'
 export type { default as SpecifierType } from './specifier-type'
 export type { default as StatementType } from './statement-type'
---
+++ src/types/mime-type.ts
@@ -0,0 +1,13 @@
+/**
+ * @file Type Definitions - MIMEType
+ * @module mlly/types/MIMEType
+ */
+
+/**
+ * Supported MIME types.
+ *
+ * @see https://nodejs.org/api/esm.html#esm_data_imports
+ */
+type MIMEType = 'application/json' | 'application/wasm' | 'text/javascript'
+
+export type { MIMEType as default }
