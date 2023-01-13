dae9081166c2af8710faef2b737d45420dd05ce4
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-01-13 14:00:32 -0500

    feat(interfaces): `GetSourceOptions`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/interfaces/__tests__/options-get-source.spec-d.ts
@@ -0,0 +1,46 @@
+/**
+ * @file Type Tests - GetSourceOptions
+ * @module mlly/interfaces/tests/unit-d/GetSourceOptions
+ */
+
+import type { Format } from '#src/enums'
+import type { ModuleId } from '#src/types'
+import type { KeysRequired } from '@flex-development/tutils'
+import type { RequestInit } from 'node-fetch'
+import type TestSubject from '../options-get-source'
+
+describe('unit-d:interfaces/GetSourceOptions', () => {
+  it('should allow empty object', () => {
+    expectTypeOf<KeysRequired<TestSubject>>().toBeNever()
+  })
+
+  it('should match [base?: ModuleId]', () => {
+    expectTypeOf<TestSubject>()
+      .toHaveProperty('base')
+      .toEqualTypeOf<ModuleId | undefined>()
+  })
+
+  it('should match [experimental_network_imports?: boolean]', () => {
+    expectTypeOf<TestSubject>()
+      .toHaveProperty('experimental_network_imports')
+      .toEqualTypeOf<boolean | undefined>()
+  })
+
+  it('should match [format?: Format | Lowercase<keyof typeof Format>]', () => {
+    expectTypeOf<TestSubject>()
+      .toHaveProperty('format')
+      .toEqualTypeOf<Format | Lowercase<keyof typeof Format> | undefined>()
+  })
+
+  it('should match [ignore_errors?: boolean]', () => {
+    expectTypeOf<TestSubject>()
+      .toHaveProperty('ignore_errors')
+      .toEqualTypeOf<boolean | undefined>()
+  })
+
+  it('should match [req?: RequestInit]', () => {
+    expectTypeOf<TestSubject>()
+      .toHaveProperty('req')
+      .toEqualTypeOf<RequestInit | undefined>()
+  })
+})
---
+++ src/interfaces/options-get-source.ts
@@ -0,0 +1,70 @@
+/**
+ * @file Interfaces - GetSourceOptions
+ * @module mlly/interfaces/GetSourceOptions
+ */
+
+import type { Format } from '#src/enums'
+import type { ModuleId } from '#src/types'
+import type { RequestInit } from 'node-fetch'
+
+/**
+ * Source code retrieval options.
+ *
+ * @see {@linkcode Format}
+ * @see {@linkcode ModuleId}
+ * @see {@linkcode RequestInit}
+ */
+interface GetSourceOptions {
+  /**
+   * Base URL to resolve module id against if module id is not absolute.
+   *
+   * @see {@linkcode ModuleId}
+   *
+   * @default undefined
+   */
+  base?: ModuleId | undefined
+
+  /**
+   * Enable support for network based modules.
+   *
+   * **Note**: Requires [`node-fetch`][1].
+   *
+   * [1]: https://github.com/node-fetch/node-fetch
+   *
+   * @see https://nodejs.org/docs/latest-v16.x/api/esm.html#https-and-http-imports
+   *
+   * @default false
+   */
+  experimental_network_imports?: boolean | undefined
+
+  /**
+   * Module format hint.
+   *
+   * @see {@linkcode Format}
+   *
+   * @default undefined
+   */
+  format?: Format | Lowercase<keyof typeof Format> | undefined
+
+  /**
+   * Ignore [`ERR_UNSUPPORTED_ESM_URL_SCHEME`][1] if thrown.
+   *
+   * [1]: https://nodejs.org/api/errors.html#err_unsupported_esm_url_scheme
+   *
+   * @default false
+   */
+  ignore_errors?: boolean | undefined
+
+  /**
+   * Request options for network based modules.
+   *
+   * Only applicable if {@linkcode experimental_network_imports} is enabled.
+   *
+   * @see {@linkcode RequestInit}
+   *
+   * @default undefined
+   */
+  req?: RequestInit | undefined
+}
+
+export type { GetSourceOptions as default }
