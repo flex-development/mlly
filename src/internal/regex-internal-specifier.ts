6f1b8be5ea341c279c57176d1af8eb9effca0c7a
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-01-17 20:03:08 -0500

    feat(internal): `INTERNAL_SPECIFIER_REGEX`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

---
+++ src/internal/regex-internal-specifier.ts
@@ -0,0 +1,16 @@
+/**
+ * @file Internal - INTERNAL_SPECIFIER_REGEX
+ * @module mlly/internal/INTERNAL_SPECIFIER_REGEX
+ */
+
+/**
+ * Regex pattern matching subpath imports.
+ *
+ * @see https://regex101.com/r/ppVRw7
+ * @see https://nodejs.org/api/packages.html#subpath-imports
+ *
+ * @const {RegExp} INTERNAL_SPECIFIER_REGEX
+ */
+const INTERNAL_SPECIFIER_REGEX: RegExp = /^#[^\s/].+[^/]$/
+
+export default INTERNAL_SPECIFIER_REGEX
